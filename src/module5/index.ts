fad70b62cb1d5b8c85cedb4720b43c2d74694158
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2023-06-23 13:01:27 +0600

    utilities added

---
+++ src/module5/index.ts
@@ -0,0 +1,12 @@
+// import Jekono from "./module";
+// import addTwo from './utils/add'
+// import multiply from './utils/multiply'
+// import average from './utils/average'
+import methods from "./utils/index";
+
+const add = (param1: number, param2: number, param3: number): number => {
+  return param1 + param2 + param3;
+};
+
+const res1 = methods.addTwo(4, 6);
+const res2 = methods.multiply(4, 6);
---
+++ src/module5/utils/index.ts
@@ -0,0 +1,7 @@
+import addTwo from "./add";
+import multiply from "./multiply";
+
+export default {
+  addTwo,
+  multiply,
+};
